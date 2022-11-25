49cc14a92fe9789bc84326e72271db7740732b85
josegroba <josedanielgroba@gmail.com>
2022-11-25 21:13:37 +0100

    25/11/2022

---
+++ docker-php/html/DWCS/UD1/numerosObj/Adivinar.php
@@ -0,0 +1,18 @@
+<?php
+class Adivinar{
+    protected $numeros;
+    protected $numero;
+    protected $vidas;
+    protected $numsecreto;
+    protected $mensaje;
+    
+    public function __construct(){
+        
+    }
+    public function juega($num){
+
+    }
+
+}
+
+?>
---
+++ docker-php/html/DWCS/UD1/numerosObj/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adivina el número</title>
+    <style>
+    </style>
+</head>
+<body>
+    <h1>Juego de adivinar el número</h1>
+    <form action="" method="post">
+        <input type="number" name="num" id="num">
+        <input type="submit" value="guardar">    
+    </form>
+</body>
+</html>
